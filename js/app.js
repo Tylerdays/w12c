c969008f3596b4b37b37b530757fed978b4479b3
Tylerdays <tylerdday.software@gmail.com>
2022-11-26 16:46:18 -0700

    got he api posts to load on page start up

--- js/app.js
+++ js/app.js
@@ -7,7 +7,25 @@ function submitPost(){
             body : document.getElementById(`postBodyBox`).value,
             userId : document.getElementById(`userIdBox`).value
         }
-    }).then(postSuccess).catch(postFailure);
+    }).then(postSuccess).catch(apiFailure);
+}
+
+function loadPosts(){
+    axios.request({
+        url : `https://jsonplaceholder.typicode.com/posts`,
+        method: `GET`,
+    }).then(loadSuccess).catch(apiFailure)
+}
+
+function loadSuccess(response){
+    let loaded = response.data;
+    for (data of loaded){
+        document.getElementById(`oldPosts`).insertAdjacentHTML(`beforeend`, `<h3>${data.id}</h3>`),
+        document.getElementById(`oldPosts`).insertAdjacentHTML(`beforeend`, `<h3>${data.userId}</h3>`),
+        document.getElementById(`oldPosts`).insertAdjacentHTML(`beforeend`, `<h3>${data.title}</h3>`),
+        document.getElementById(`oldPosts`).insertAdjacentHTML(`beforeend`, `<h3>${data.body}</h3>`)
+    }
+
 }
 
 function postSuccess(response){
@@ -18,8 +36,8 @@ function postSuccess(response){
     console.log(data);
 }
 
-function postFailure(error){
-    document.body.insertAdjacentHTML(`beforeend`, `<h3>Post has failed</h3>`);
+function apiFailure(error){
+    document.body.insertAdjacentHTML(`beforeend`, `<h3>Failed to Post</h3>`);
     console.log(error);
 }
 
@@ -27,7 +45,27 @@ function clearPost(){
     postContainer.innerHTML = "";
 }
 
+function clearOld(){
+    document.getElementById(`oldPosts`).innerHTML = "";
+}
+
+/**function editPost(){
+    axios.request({
+        url : `https://jsonplaceholder.typicode.com/posts`,
+        method: `PATCH`,
+        data: {
+            title : document.getElementById(`postTitleBox`).value,
+            body : document.getElementById(`postBodyBox`).value,
+            userId : document.getElementById(`userIdBox`).value
+        }
+    }).then(editSuccess).catch(apiError)
+} */
+
+
 const postContainer = document.getElementById(`results`);
 
 document.getElementById(`submitPost`).addEventListener(`click`, submitPost);
+document.addEventListener(`readystatechange`, loadPosts);
 document.getElementById(`clearPost`).addEventListener(`click`, clearPost);
+document.getElementById(`clearOld`).addEventListener(`click`, clearOld);
+//document.getElementById(`editPost`).addEventListener(`click`, editPost);//
